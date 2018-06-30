f80c3cfcdab0bade73ae916bb4f444ebd99ae23a
venkat <venkat@gmail.com>
2018-06-30 14:52:11 +0530

    second

---
+++ Set and List.py
@@ -0,0 +1,4 @@
+l=[1,2,3,1,4,5,6,8,9]
+print(l)
+print(set(l))
+print(tuple(l))
---
+++ first prg.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 21 10:13:48 2018
+
+@author: Admin
+"""
+
+x=int(input("Enter Any Number:" ))
+if x>=0:
+    print("x is +ve")
+else:
+    print("x is -ve")
